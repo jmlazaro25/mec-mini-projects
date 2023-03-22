f10d259c026686e849ed55d7bdda9b3012c52034
jmlazaro <jmlazaro@localhost.localdomain>
2023-03-21 20:04:43 -0700

    5.5.4 v1

---
+++ mec-5.5.4-webscraping-project/smp/smp/spiders/quotes_spider.py
@@ -0,0 +1,67 @@
+import scrapy
+
+
+class CSSQuotesSpider(scrapy.Spider):
+    name = "toscrape-css"
+
+    def start_requests(self):
+        url = 'https://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        for a in response.css('ul.pager a'):
+            yield response.follow(a, callback=self.parse)
+
+class XpathQuotesSpider(scrapy.Spider):
+    name = "toscrape-xpath"
+
+    def start_requests(self):
+        url = 'https://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
+
+    def parse(self, response):
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield {
+                'text': quote.xpath('./span[@class="text"]/text()').get(),
+                'author': quote.xpath('.//small[@class="author"]/text()').get(),
+                'tags': quote.xpath('./div[@class="tags"]/a[@class="tag"]/text()').getall(),
+            }
+
+        for a in response.xpath('//li[@class="next"]/a/@href').getall():
+            yield response.follow(a, callback=self.parse)
+
+class AuthorSpider(scrapy.Spider):
+    name = 'author'
+
+    start_urls = ['https://quotes.toscrape.com/']
+
+    def parse(self, response):
+        author_page_links = response.css('.author + a')
+        yield from response.follow_all(author_page_links, self.parse_author)
+
+        pagination_links = response.css('li.next a')
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
+
